b237cea040bf359b43c7bbbe4366f401dae8c1ab
sparuchuri <sparuchuri@lifetouch.com>
2019-07-17 15:30:40 -0500

    intial commit

---
+++ src/test/java/com/mpalourdio/html5/fake/UselessTest.java
@@ -0,0 +1,13 @@
+package com.mpalourdio.html5.fake;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class UselessTest {
+
+    @Test
+    void testOneEqualsOne() {
+        assertEquals(1, 1);
+    }
+}
